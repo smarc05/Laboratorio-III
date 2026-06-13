e62412f8452a017e4d4f850bf29313fc111c0a9e
Marcos <marcosromansalas@gmail.com>
2026-06-12 23:18:16 -0300

    Puse las bibliotecas y agrege la URL del repo

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * Informatica II
+ * Laboratorio III: Uso de semáforos y pipes entre procesos en Linux
+ * Repositorio: <https://github.com/smarc05/Laboratorio-III.git>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>  
+#include <sys/types.h>
+#include <sys/wait.h> 
+#include <sys/mman.h> 
+#include <semaphore.h>
+#include <fcntl.h>    
+
+int main() 
+{
+    
+    return 0;
+}
